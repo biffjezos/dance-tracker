6e1844e11d24eb172bf10f08bc9b6b9262f89526
biffjezos <biff.jezos@gmail.com>
2026-07-28 23:05:51 +0200

    wasm.js

--- ui/scripts/core/wasm.js
+++ ui/scripts/core/wasm.js
@@ -10,28 +10,21 @@ instance used by graph/render/output modules.
 ==================================================
 */
 
-// import init, { App } from "../../core/pkg/dance_tracker_core.js";
-// import { WIDTH, HEIGHT } from "../engine/constants.js";
+import init, { App } from "../../core/pkg/dance_tracker_core.js";
+import { WIDTH, HEIGHT } from "../engine/constants.js";
 
-let wasmApp = null;
-
+export let wasmApp = null;
 
 export async function initWasm() {
-    // if (wasmApp) return wasmApp;
+    if (wasmApp) return wasmApp;
 
-    // await init();
+    await init();
 
-    // wasmApp = new App(
-    //     WIDTH,
-    //     HEIGHT
-    // );
-
-    // return wasmApp;
-    return null;
+    wasmApp = new App( WIDTH, HEIGHT );
+    return wasmApp;
 }
 
 
 export function getWasmApp() {
-    // return wasmApp;
-    return null;
+    return wasmApp;
 }
