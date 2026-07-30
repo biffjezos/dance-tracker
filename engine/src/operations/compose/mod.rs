c0f4a2594e0d3721ea480937be1228830d65ab71
biffjezos <biff.jezos@gmail.com>
2026-07-30 18:59:34 +0200

    use imports

---
+++ engine/src/operations/compose/mod.rs
@@ -0,0 +1,5 @@
+pub mod merge;
+pub mod multiply;
+
+pub use merge::Merge;
+pub use multiply::Multiply;
--- engine/src/operations/transform/mod.rs
+++ engine/src/operations/transform/mod.rs
@@ -1,10 +1,6 @@
 // src/operations/transform/mod.rs
 pub mod blur;
-pub mod merge;
-pub mod multiply;
 pub mod shuffle;
 
 pub use blur::Blur;
-pub use merge::Merge;
-pub use multiply::Multiply;
 pub use shuffle::Shuffle;
