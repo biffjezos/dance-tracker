e5c7ee7690f43754b46e6ccc9d2950f0112b5627
biffjezos <biff.jezos@gmail.com>
2026-08-03 14:19:12 +0200

    biffjezos: add bind gorups

--- engine/src/gpu/pipeline.rs
+++ engine/src/gpu/pipeline.rs
@@ -16,7 +16,42 @@ impl BlurPipeline {
                 .create_bind_group_layout(
                     &wgpu::BindGroupLayoutDescriptor {
                         label: Some("blur bind group layout"),
-                        entries: &[],
+                        entries: &[
+                            wgpu::BindGroupLayoutEntry {
+                                binding: 0,
+                                visibility: wgpu::ShaderStages::COMPUTE,
+                                ty: wgpu::BindingType::Buffer {
+                                    ty: wgpu::BufferBindingType::Storage {
+                                        read_only: true,
+                                    },
+                                    has_dynamic_offset: false,
+                                    min_binding_size: None,
+                                },
+                                count: None,
+                            },
+                            wgpu::BindGroupLayoutEntry {
+                                binding: 1,
+                                visibility: wgpu::ShaderStages::COMPUTE,
+                                ty: wgpu::BindingType::Buffer {
+                                    ty: wgpu::BufferBindingType::Storage {
+                                        read_only: false,
+                                    },
+                                    has_dynamic_offset: false,
+                                    min_binding_size: None,
+                                },
+                                count: None,
+                            },
+                            wgpu::BindGroupLayoutEntry {
+                                binding: 2,
+                                visibility: wgpu::ShaderStages::COMPUTE,
+                                ty: wgpu::BindingType::Buffer {
+                                    ty: wgpu::BufferBindingType::Uniform,
+                                    has_dynamic_offset: false,
+                                    min_binding_size: None,
+                                },
+                                count: None,
+                            },
+                        ],
                     },
                 );
 
