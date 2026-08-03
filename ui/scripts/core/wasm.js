787bca110d8b27454503d29a0c41b39e2ca6c689
biffjezos <biff.jezos@gmail.com>
2026-08-04 00:19:29 +0200

    Add systemMenus export to wasm.js

--- ui/scripts/core/wasm.js
+++ ui/scripts/core/wasm.js
@@ -4,7 +4,7 @@ import init, { App } from "../../engine/pkg/dance_tracker_engine.js";
 import { WIDTH, HEIGHT } from "../engine/constants.js";
 
 export let wasmApp = null;
-
+export let systemMenus = null;
 export async function initWasm() {
     if (wasmApp) return wasmApp;
 
@@ -15,7 +15,7 @@ export async function initWasm() {
     wasmApp = App.new(WIDTH, HEIGHT);
 
     console.log("Created WASM App:", wasmApp);
-
+    systemMenus = wasmApp.get_system_menus();
     return wasmApp;
 }
 
